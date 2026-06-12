fb63b29126385ff43adb7bd77214314891285e2c
George Glor <georgeglor40@hotmail.com>
2026-06-12 21:23:12 +0200

    fixing website1

--- components/landing/Ambient3D.tsx
+++ components/landing/Ambient3D.tsx
@@ -37,6 +37,51 @@ function Orb({ position, color, scale, speed, distort, opacity, animated }: OrbP
   );
 }
 
+function BgReceipt({
+  position,
+  scale,
+  speed,
+  animated,
+}: {
+  position: [number, number, number];
+  scale: number;
+  speed: number;
+  animated: boolean;
+}) {
+  const lines = [1.5, 1.2, 0.9, 0.6, 0.3, 0.0, -0.3];
+  return (
+    <Float
+      speed={animated ? speed : 0}
+      rotationIntensity={animated ? 0.8 : 0}
+      floatIntensity={animated ? 1.4 : 0}
+    >
+      <group position={position} scale={scale} rotation={[0.1, -0.4, 0.12]}>
+        {/* paper */}
+        <mesh>
+          <boxGeometry args={[2.2, 4, 0.04]} />
+          <meshStandardMaterial color="#FBF9F4" roughness={0.9} transparent opacity={0.92} />
+        </mesh>
+        {/* header bar */}
+        <mesh position={[0, 1.85, 0.06]}>
+          <boxGeometry args={[1.4, 0.16, 0.01]} />
+          <meshStandardMaterial color="#2F6079" />
+        </mesh>
+        {lines.map((y, i) => (
+          <mesh key={i} position={[-0.35 + ((i * 0.07) % 0.2), y, 0.06]}>
+            <boxGeometry args={[1.1 - (i % 3) * 0.25, 0.06, 0.01]} />
+            <meshStandardMaterial color="#C7C0B0" />
+          </mesh>
+        ))}
+        {/* total bar */}
+        <mesh position={[0, -0.75, 0.06]}>
+          <boxGeometry args={[1.6, 0.12, 0.01]} />
+          <meshStandardMaterial color="#16181D" />
+        </mesh>
+      </group>
+    </Float>
+  );
+}
+
 function Scene({ animated }: { animated: boolean }) {
   const group = useRef<Group>(null);
   useFrame((state) => {
@@ -53,6 +98,9 @@ function Scene({ animated }: { animated: boolean }) {
       <Orb position={[3.8, -1.4, -3]} color="#D98A37" scale={1.2} speed={0.85} distort={0.3} opacity={0.42} animated={animated} />
       <Orb position={[2.6, 2.4, -4]} color="#2F6079" scale={0.9} speed={1.3} distort={0.4} opacity={0.5} animated={animated} />
       <Orb position={[-2.8, -2.2, -3.5]} color="#5B8AA6" scale={0.75} speed={1.0} distort={0.45} opacity={0.45} animated={animated} />
+      {/* Floating bills drifting in the background */}
+      <BgReceipt position={[-4.2, -0.6, -3]} scale={0.5} speed={1.0} animated={animated} />
+      <BgReceipt position={[4.4, 1.0, -4]} scale={0.38} speed={0.8} animated={animated} />
     </group>
   );
 }
